ae0b9db51ecc852da4e3d333e9930ef49aea9212
Julia Grigorenko <jvgrigorenko@gmail.com>
2020-12-15 21:33:28 +0300

    update GET / route with data from database

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,20 @@
 const router = require('express').Router();
 const date = require(__dirname + '/../libs/date.js');
+const mongoose = require (__dirname + '/../libs/mongoose');
+let Item = require(__dirname + '/../models/item.model.js');
+let List = require(__dirname + '/../models/list.model.js');
 
 let day = date();
 
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 router.route('/').get((req, res) => {
-
-    res.render("list", {listTitle: day, listName: 'average', newListItems: []});
+  Item.Item.find({}, (err, foundItems) => {
+    res.render("list", {listTitle: day, listName: 'average', newListItems: foundItems});
+  });
 });
 
 module.exports = router;
