52d813997084d0b6315cd7fef766bed562f0c4a8
Julia Grigorenko <jvgrigorenko@gmail.com>
2020-12-15 21:36:37 +0300

    add GET /:customListName route

--- routes/notes.js
+++ routes/notes.js
@@ -3,6 +3,7 @@ const date = require(__dirname + '/../libs/date.js');
 const mongoose = require (__dirname + '/../libs/mongoose');
 let Item = require(__dirname + '/../models/item.model.js');
 let List = require(__dirname + '/../models/list.model.js');
+const _ = require("lodash");
 
 let day = date();
 
@@ -14,6 +15,20 @@ connection.once('open', () => {
 router.route('/').get((req, res) => {
   Item.Item.find({}, (err, foundItems) => {
     res.render("list", {listTitle: day, listName: 'average', newListItems: foundItems});
+  });
+});
+
+router.route('/:customListName').get((req, res) => {
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.List.findOne({name: customListName}, (err, foundList) => {
+    if (!err){
+      if (!foundList){
+          res.render("list", {listTitle: day, listName: customListName, newListItems: []});
+      } else {
+          res.render("list", {listTitle: day, listName:foundList.name, newListItems: foundList.items});
+      }
+    }
   });
 });
 
