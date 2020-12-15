7eec89877fe443c5b55044879c518e3ed4babe95
Julia Grigorenko <jvgrigorenko@gmail.com>
2020-12-15 20:54:20 +0300

    set ejs view engine, add ejs templates

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5005;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('hello');
-});
+  res.render("list");
+  });
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
